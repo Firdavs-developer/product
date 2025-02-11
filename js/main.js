4f5bc117ec177556afba9626de20542e9efc2d57
Firdavs Developer <firdavsdosiyorov3@gmail.com>
2025-02-11 15:07:26 +0500

    initaliz commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let elBtn = document.querySelector(".mode-btn");
+
+elBtn.addEventListener("click", () => {
+    if (localStorage.getItem("theme") === "dark" || (!localStorage.getItem("theme") && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+        document.documentElement.classList.remove("dark"); // O‘chirish
+        localStorage.setItem("theme", "light"); // Yangi holatni saqlash
+    } else {
+        document.documentElement.classList.add("dark"); // Qo‘shish
+        localStorage.setItem("theme", "dark"); // Yangi holatni saqlash
+    }
+});
